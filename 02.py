a33293f5d4eb0b997c9ac56b63339f2b302cd7fd
Enes ARSLAN <enesars716@gmail.com>
2021-02-17 16:05:15 +0300

    Güncelleme 1

--- 02.py
+++ 02.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 from PIL import Image
 import os
+
 # Path for face image database
 path = 'dataset'
 recognizer = cv2.face.LBPHFaceRecognizer_create()
@@ -20,10 +21,11 @@ def getImagesAndLabels(path):
             faceSamples.append(img_numpy[y:y+h,x:x+w])
             ids.append(id)
     return faceSamples,ids
+
 print ("\n [INFO] Training faces. It will take a few seconds. Wait ...")
 faces,ids = getImagesAndLabels(path)
 recognizer.train(faces, np.array(ids))
-# Save the model into trainer/trainer.yml
+# Save the model into trainer.yml
 recognizer.write('trainer.yml') # recognizer.save() worked on Mac, but not on Pi
 # Print the numer of faces trained and end program
 print("\n [INFO] {0} faces trained. Exiting Program".format(len(np.unique(ids))))
